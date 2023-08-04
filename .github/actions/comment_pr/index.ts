fdca19686a709cea3b491b115ab4e643f57a3b9e
pngwn <hello@pngwn.io>
2023-08-04 19:41:09 +0100

    action

---
+++ .github/actions/comment_pr/index.ts
@@ -0,0 +1,43 @@
+import { join } from "path";
+import { readFile } from "fs/promises";
+import "./fetch";
+import { Blob } from "node:buffer";
+//@ts-ignore
+import recursive_read_dir from "recursive-readdir";
+
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+import * as artifact from "@actions/artifact";
+
+
+
+async function run() {
+	const token = core.getInput("token");
+	const octokit = github.getOctokit(token);	
+	const context = github.context;
+	const repo = context.repo;
+	const pr_number = context.payload.pull_request?.number;
+
+	if (!pr_number) {
+		core.setFailed("No PR number found.");
+		return;
+	}
+
+	const pr = await octokit.rest.pulls.get({
+		...repo,
+		pull_number: pr_number,
+	});
+
+	// get pr comments
+	const comments = await octokit.rest.issues.listComments({
+		...repo,
+		issue_number: pr_number,
+
+	});
+
+
+	console.log(comments)
+
+
+	
+}
