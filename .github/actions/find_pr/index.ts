299351c260059641c27ccf69a1f1b15f5e8f2eb2
pngwn <hello@pngwn.io>
2023-08-07 12:39:24 +0100

    action

--- .github/actions/find_pr/index.ts
+++ .github/actions/find_pr/index.ts
@@ -7,8 +7,8 @@ type Octokit = ReturnType<typeof github.getOctokit>
 const COMMENT_ID = 'GRADIO_GITHUB_ACTION_COMMENT_ID'
 
 async function run() {
-	const token = core.getInput("token");
-	const octokit = github.getOctokit(token);	
+// 	const token = core.getInput("token");
+// 	const octokit = github.getOctokit(token);	
 	const context = github.context;
 	console.log(JSON.stringify(context, null, 2))
 
